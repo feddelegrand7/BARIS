709f66636c1123d221f7c2e0cb87a1907b13923b
Fodil Ihaddaden <ihaddaden.fodeil@gmail.com>
2020-01-25 16:06:52 +0100

    k

---
+++ R/BARIS_data.R
@@ -0,0 +1,67 @@
+
+#' Title
+#'
+#' @return
+#' @export
+#'
+#' @examples
+#' @importFrom dplyr as_tibble
+BARIS_data <- function() {
+
+  my_url <- "https://www.data.gouv.fr/api/1/site/home/datasets/"
+
+  # set memoise cache fct
+  mem_fromJSON <- BARIS_set_fromJSON()
+
+  df_info <- mem_fromJSON(my_url)
+
+  id                 <- df_info$id
+
+  title              <- df_info$title
+
+  frequency          <- df_info$frequency
+
+  created_at         <- df_info$created_at
+
+  last_modified      <- df_info$last_modified
+
+  last_update        <- df_info$last_update
+
+  archived           <- df_info$archived
+
+  deleted            <- df_info$deleted
+
+  nb_views           <- df_info$metrics[[4]]
+
+  organization       <- df_info$organization[[6]]
+
+  temporal_cov_start <- df_info$temporal_coverage[[1]]
+
+  temporal_cov_end   <- df_info$temporal_coverage[[2]]
+
+  page               <- df_info$page
+
+  df_info2 <- cbind(
+    id,
+    title,
+    organization,
+    page,
+    views,
+    frequency,
+    temporal_cov_start,
+    temporal_cov_end,
+    created_at,
+    last_modified,
+    last_update,
+    archived,
+    deleted
+
+    )
+
+  df_info2 <- as_tibble(df_info2)
+
+  return(df_info2)
+}
+
+
+
---
+++ R/memoise.R
@@ -0,0 +1,23 @@
+
+
+#' Title
+#'
+#' @param cache_folder
+#'
+#' @return
+#' @export
+#'
+#'
+#'
+#' @examples
+#' @importFrom jsolite fromJSON
+#' @importFrom memoise memoise
+#' @importFrom memoise cache_filesystem
+BARIS_set_fromJSON <- function(cache_folder = tempdir()) {
+
+  mem_fromJSON <- memoise::memoise(jsonlite::fromJSON,
+    cache = memoise::cache_filesystem(cache_folder))
+
+  return(mem_fromJSON)
+
+}
