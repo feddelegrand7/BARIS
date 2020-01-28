9b54b3feb0a3220ce01ee6236db9f46f20fa3863
Fodil Ihaddaden <ihaddaden.fodeil@gmail.com>
2020-01-28 12:45:30 +0100

    Adding several reading possibilities to the BARIS_extract function

--- R/BARIS_extract.R
+++ R/BARIS_extract.R
@@ -9,6 +9,13 @@
 #' @importFrom dplyr as_tibble
 #' @importFrom data.table fread
 #' @importFrom reader get.ext
+#' @importFrom downloader download
+#' @importFrom sf read_sf
+#' @importFrom XML xmlParse
+#' @importFrom XML xmlToDataFrame
+#' @importFrom httr BROWSE
+#' @importFrom rgdal readOGR
+
 
 
 BARIS_CSVextract <- function(resourceId) {
@@ -24,18 +31,93 @@ BARIS_CSVextract <- function(resourceId) {
 
   resp <- httr::GET(final_url)
 
+  #### Reading CSV files ############################################################
+
   if(http_type(resp) == "csv" || http_type(resp) == "text" || http_type(resp) == "text/csv" ){
 
     resource <- data.table::fread(final_url, stringsAsFactors = F, sep = "auto")
     resource <- dplyr::as_tibble(resource)
     return(resource)
 
+  #### Reading Excel files ############################################################
+
+
   } else if (http_type(resp) == "xls" || http_type(resp) == "xlsx"){
 
 
     resource <- rio::import(final_url)
 
-  } else if (#Je dois continuer XTML et JSON)
+    return(resource)
+
+  #### Reading XML files ############################################################
+
+
+  } else if (http_type(resp) == "xml" ){
+
+
+
+    xml_info <- XML::xmlParse(final_url)
+
+    xmldf <- XML::xmlToDataFrame(nodes = getNodeSet(xml_info, "//record"))
+
+    return(xmldf)
+
+  #### Reading SHP files ############################################################
+
+
+  } else if (http_type(resp) == "shp" || http_type(resp) == "shapefile"){
+
+    temp <- tempfile()
+    temp2 <- tempfile()
+
+    downloader::download(final_url, dest=temp, mode="wb")
+
+    unzip(zipfile = temp, exdir = temp2)
+
+    SHP_file <- list.files(temp2, pattern = ".shp$",full.names=TRUE)
+
+    return(sf::read_sf(SHP_file))
+
+    unlink(c(temp, temp2))
+
+
+  #### Reading PDF files ############################################################
+
+
+  } else if (http_type(resp) == "pdf"){
+
+    httr::BROWSE(final_url)
+
+  #### Downloading ZIP file ############################################################
+
+
+  } else if (http_type(resp) == "zip"){
+
+    httr::BROWSE(final_url)
+
+  #### Downloading GEOJSON file ############################################################
+
+
+  } else if (http_type(resp) == "geojson"){
+
+
+    temp <- tempfile()
+    temp2 <- tempfile()
+
+    downloader::download(final_url, dest=temp, mode="wb")
+
+    unzip(zipfile = temp, exdir = temp2)
+
+    SHP_file <- list.files(temp2, pattern = "geojson$",full.names=TRUE)
+
+    return(rgdal::readOGR(SHP_file, layer = "OGRGeoJSON"))
+
+    unlink(c(temp, temp2))
+
+
+  }
+
+
 
 
 
